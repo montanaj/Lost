bd5b89ab57eb46e6cc8b7ca8306190dcc318ee28
montanaj <clairejencks@gmail.com>
2014-04-01 14:32:41 -0500

    first commit

---
+++ RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Lost
+//
+//  Created by Claire Jencks on 4/1/14.
+//  Copyright (c) 2014 Claire Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ RootViewController.m
@@ -0,0 +1,56 @@
+//
+//  RootViewController.m
+//  Lost
+//
+//  Created by Steve Toosevich on 4/1/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) IBOutlet UIToolbar *characterNameTextField;
+@property (strong, nonatomic) IBOutlet UITableView *myTableView;
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"LostCharactersReuseID"];
+    return cell;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
